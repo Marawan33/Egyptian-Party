ea76001676eeff2ed04f05ecc577e635ed77c5e9
Marawan33 <marawan.mohamed112233@gmail.com>
2024-01-22 21:57:35 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,107 @@
+
+/*leftmenu*/
+
+$(".openNav").click(function(){
+    $("#leftMenu").animate({ width:'250px'},50)
+   $("#home-content").animate({marginLeft :'250px'},50)
+})
+
+$(".closebtn").click(function(){
+    $("#leftMenu").animate({ width:'0px'},50)
+   $("#home-content").animate({marginLeft :'0px'},50)
+})
+
+
+
+/*scrollmenu*/
+$("#leftMenu a").click(function(){
+    
+    var sectionId= $(this).attr("href");
+    
+    var positionOfSection = $(sectionId).offset().top;
+    
+    $("html , body").animate({scrollTop:positionOfSection});
+    
+})
+
+
+
+/*Accordion*/
+
+
+$('#sliderDown .toggle').click(function(){
+    $('.inner').not($(this).next()).slideUp();
+    $(this).next().slideToggle();
+});
+
+
+
+/*counter*/
+
+
+window.onload = function() {
+   
+    countDownToTime("30 january 2024 10:00:00");
+  }
+
+  function countDownToTime(countTo) {
+  
+        let futureDate = new Date(countTo);
+    futureDate = (futureDate.getTime()/1000);
+
+    let now = new Date();
+    now = (now.getTime()/1000);
+
+    timeDifference = (futureDate- now);
+        
+   let days = Math.floor( timeDifference / (24*60*60));
+   let hours = Math.floor((timeDifference - (days * (24*60*60))) / 3600);
+   let mins = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600 )) / 60);
+   let secs = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600) - (mins * 60)))
+
+
+    $(".days").html(`${days} D`);
+    $(".hours").html(`${hours} h`);
+    $(".minutes").html(`${ mins } m`);
+    $('.seconds').html(`${ secs} s`)
+
+  
+    setInterval(function() {  countDownToTime(countTo); }, 1000);
+  }
+
+
+
+/*textarea*/
+
+var maxLength = 100;
+$('textarea').keyup(function() {
+  var length = $(this).val().length;
+  var AmountLeft = maxLength-length;
+  if(AmountLeft<=0)
+            {
+                 $("#chars").text("you have entered more than 100 character");
+                
+            }
+        else{
+        
+        $("#chars").text(AmountLeft);
+        }
+});
+
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+    }
